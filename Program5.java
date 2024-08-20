dc1f5fea8cf4b30994898e7652fdfad8bfdb5269
Ani7814 <anir7814@gmail.com>
2024-08-20 15:30:43 -0700

    Update Program5.java
    Partially completed program 5

--- Program5.java
+++ Program5.java
@@ -1,7 +1,44 @@
 //import stuff here?
 
 //Your code here
-
+public class program5{
+    public static void main(String[] args){
+        int royaleMiles = 286;
+        int royaleGallons = 9;
+        int koopaKingMiles = 412;
+        int koopaKingGallons = 40;
+        int pipeFrameMiles = 361;
+        int pipeFrameGallons = 18;
+        int badwagonMiles = 161;
+        int badwagonGallons = 11;
+        System.out.println("Mushroom Cup Prix Racer Average Miles/Per Gallon: ");
+        System.out.println("Royale averaged "+ Math.round(10*(double)royaleMiles/royaleGallons)/10.0+"m/s");
+        System.out.println("Koopa King averaged "+ Math.round(10*(double)koopaKingMiles/koopaKingGallons)/10.0+"m/s");
+        System.out.println("Pipe frame averaged "+ Math.round(10*(double)pipeFrameMiles/pipeFrameGallons)/10.0+"m/s");
+        System.out.println("Badwagon averaged "+ Math.round(10*(double)badwagonMiles/badwagonGallons)/10.0+"m/s");
+        /*double royaleMilePerGallon = (double) royaleMiles/royaleGallons*10;
+        int royaleMilesPerGallon = (int) royaleMilePerGallon+0.5;
+        double royaleMilesperGallon = (double) royaleMilesPerGallon/10;
+        System.out.println("Royale averaged "+ royaleMilesperGallon +" m/g");
+        //System.out.printf("%.2f", royaleMilePerGallon);
+        double koopaKingMilePerGallon = (double) koopaKingMiles/koopaKingGallons;
+        double pipeFrameMilePerGallon = (double) pipeFrameMiles/pipeFrameGallons;
+        double badwagonMilePerGallon = (double) badwagonMiles/badwagonGallons;
+        System.out.print("Koopa King averaged ");
+        System.out.printf("%.1f", koopaKingMilePerGallon);
+        System.out.println("m/s");
+        System.out.print("Pipe Frame averaged ");
+        System.out.printf("%.1f", pipeFrameMilePerGallon);
+        System.out.println("m/s");
+        System.out.print("Badwagon averaged ");
+        System.out.printf("%.1f", badwagonMilePerGallon);
+        System.out.print("m/s");*/
+        
+        
+    
+    
+    }
+}
 //Paste console output below:
 /*
 
