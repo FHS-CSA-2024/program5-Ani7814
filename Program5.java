bc6f428e859b467f3baa3a0cb8fa83f45c50410b
Ani7814 <anir7814@gmail.com>
2024-08-20 18:31:16 -0700

    Update Program5.java
    Completed program 5

--- Program5.java
+++ Program5.java
@@ -1,6 +1,4 @@
-//import stuff here?
 
-//Your code here
 public class program5{
     public static void main(String[] args){
         int royaleMiles = 286;
@@ -16,31 +14,15 @@ public class program5{
         System.out.println("Koopa King averaged "+ Math.round(10*(double)koopaKingMiles/koopaKingGallons)/10.0+"m/s");
         System.out.println("Pipe frame averaged "+ Math.round(10*(double)pipeFrameMiles/pipeFrameGallons)/10.0+"m/s");
         System.out.println("Badwagon averaged "+ Math.round(10*(double)badwagonMiles/badwagonGallons)/10.0+"m/s");
-        /*double royaleMilePerGallon = (double) royaleMiles/royaleGallons*10;
-        int royaleMilesPerGallon = (int) royaleMilePerGallon+0.5;
-        double royaleMilesperGallon = (double) royaleMilesPerGallon/10;
-        System.out.println("Royale averaged "+ royaleMilesperGallon +" m/g");
-        //System.out.printf("%.2f", royaleMilePerGallon);
-        double koopaKingMilePerGallon = (double) koopaKingMiles/koopaKingGallons;
-        double pipeFrameMilePerGallon = (double) pipeFrameMiles/pipeFrameGallons;
-        double badwagonMilePerGallon = (double) badwagonMiles/badwagonGallons;
-        System.out.print("Koopa King averaged ");
-        System.out.printf("%.1f", koopaKingMilePerGallon);
-        System.out.println("m/s");
-        System.out.print("Pipe Frame averaged ");
-        System.out.printf("%.1f", pipeFrameMilePerGallon);
-        System.out.println("m/s");
-        System.out.print("Badwagon averaged ");
-        System.out.printf("%.1f", badwagonMilePerGallon);
-        System.out.print("m/s");*/
         
-        
-    
-    
     }
 }
 //Paste console output below:
 /*
-
+Mushroom Cup Prix Racer Average Miles/Per Gallon: 
+Royale averaged 31.8m/s
+Koopa King averaged 10.3m/s
+Pipe frame averaged 20.1m/s
+Badwagon averaged 14.6m/s
 
 */
